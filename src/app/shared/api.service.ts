6e23e4f9b078f422b1467a754dbf27a4047d20b6
iquallinfo <work@i-quall.com>
2022-04-29 11:06:06 +0530

    change done

--- src/app/shared/api.service.ts
+++ src/app/shared/api.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators'
-// const url = 'https://jsonplaceholder.typicode.com/';
-const url = 'http://localhost:3000/';
+const url = 'https://jsonplaceholder.typicode.com/';
+// const url = 'http://localhost:3000/';
 
 @Injectable({
   providedIn: 'root'
